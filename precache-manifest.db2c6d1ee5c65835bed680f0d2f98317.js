e84f2b89d008df40f43d1265d5a273a07903f89e
basil-kawak <basilalisa0@gmail.com>
2020-11-28 03:08:41 +0300

    Updates

---
+++ precache-manifest.db2c6d1ee5c65835bed680f0d2f98317.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfb23cee0772331746e142b40a60b9a",
+    "url": "/suicide-prevention-project/index.html"
+  },
+  {
+    "revision": "9ca3d939c8fe70ed9517",
+    "url": "/suicide-prevention-project/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ef40a2ea43aadd2e3327",
+    "url": "/suicide-prevention-project/static/css/main.ae53bda2.chunk.css"
+  },
+  {
+    "revision": "9ca3d939c8fe70ed9517",
+    "url": "/suicide-prevention-project/static/js/2.78f530d5.chunk.js"
+  },
+  {
+    "revision": "975e4581415556b768d6e0fdc3dbb681",
+    "url": "/suicide-prevention-project/static/js/2.78f530d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef40a2ea43aadd2e3327",
+    "url": "/suicide-prevention-project/static/js/main.ae31664a.chunk.js"
+  },
+  {
+    "revision": "a1b967779ef298fdf4fd",
+    "url": "/suicide-prevention-project/static/js/runtime-main.6b6ad843.js"
+  },
+  {
+    "revision": "fc0317c1089362b2930a935cfb36ac08",
+    "url": "/suicide-prevention-project/static/media/contact-page.fc0317c1.png"
+  },
+  {
+    "revision": "1b708b79a07196f05e1957f1154643fb",
+    "url": "/suicide-prevention-project/static/media/contact-us-form.1b708b79.png"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/suicide-prevention-project/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "55d3560e93ba6e8e604f43b0a089c3c3",
+    "url": "/suicide-prevention-project/static/media/googleIcon.55d3560e.png"
+  },
+  {
+    "revision": "6416ac19ea288b2785876cd944631d0f",
+    "url": "/suicide-prevention-project/static/media/infoSign.6416ac19.png"
+  },
+  {
+    "revision": "09e0c40fa909804fb2030ed0fd259ca2",
+    "url": "/suicide-prevention-project/static/media/profileHeaderBackground.09e0c40f.png"
+  },
+  {
+    "revision": "df8b2097caa3ed357f964cb8f38504c1",
+    "url": "/suicide-prevention-project/static/media/staff0.df8b2097.png"
+  },
+  {
+    "revision": "85c0604a98ec11e1cf285ffb7c04e9f0",
+    "url": "/suicide-prevention-project/static/media/staff1.85c0604a.png"
+  },
+  {
+    "revision": "9f29c504c07e7e906463660e2c68935e",
+    "url": "/suicide-prevention-project/static/media/staff2.9f29c504.png"
+  }
+]);
